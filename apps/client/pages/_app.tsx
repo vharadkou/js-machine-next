13ee30b2fbc2c2c39f05aa4892c6a3d650784613
Valiantsin <valiantsin.haradkou@icloud.com>
2022-01-11 18:47:49 +0300

    fix: favicon

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -3,18 +3,19 @@ import Head from 'next/head';
 import './styles.css';
 import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
-import { store } from '../redux/store';
 
+import { store } from '../redux/store';
 import { Layout } from '../components/Layout/Layout';
 import { theme } from '../components/Theme';
+import FaviconIco from '../public/favicon.ico';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>JS Machine App</title>
-        <link rel="icon" type="image/x-icon" href="favicon.ico" />
-        <link rel="apple-touch-icon" href="favicon.ico" />
+        <link rel="icon" type="image/x-icon" href={FaviconIco.src} />
+        <link rel="apple-touch-icon" href={FaviconIco.src} />
       </Head>
       <main>
         <ThemeProvider theme={theme}>
