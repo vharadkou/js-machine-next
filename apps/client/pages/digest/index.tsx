7bca35e218802acbbb1dd3615cc29453a2db09ed
Valiantsin <valiantsin.haradkou@icloud.com>
2022-01-21 01:21:02 +0300

    feat: update view for digest list

--- apps/client/pages/digest/index.tsx
+++ apps/client/pages/digest/index.tsx
@@ -1,11 +1,33 @@
+import { useMemo } from 'react';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import { Shell } from '../../components/Layout/Shell';
 import { useGetDigestsQuery } from '../../redux/service';
 import { DigestCard } from '../../components/DigestCard';
 import DigestJpg from '../../public/images/digest.jpg';
+import { Digest } from '../../redux/models';
 
 function Digest() {
   const { data, isLoading } = useGetDigestsQuery('');
+
+  const mappedData = useMemo(
+    () =>
+      data
+        ? data.reduce<{ [key: string]: Digest[] }>((prev, current) => {
+            const year = new Date(current.date).getFullYear();
+            const month = new Date(current.date)
+              .toLocaleString('ru', { month: 'long' })
+              .toUpperCase();
+
+            const key = `${year} ${month}`;
+            prev[key] = prev[key] ? [...prev[key], current] : [current];
+
+            return prev;
+          }, {})
+        : [],
+    [data]
+  );
 
   return (
     <Shell
@@ -18,18 +40,25 @@ function Digest() {
         justifyContent: 'center',
       }}
     >
-      <Grid
-        container
-        direction="row"
-        sx={{ justifyContent: { sx: 'start', md: 'center' }, mb: 2 }}
-        spacing={2}
-      >
-        {data?.map((digest) => (
-          <Grid key={digest.id} item xs={12} sm={6} md={4} lg={3}>
-            <DigestCard digest={digest} sx={{ height: '100%' }} />
+      {Object.keys(mappedData).map((key) => (
+        <Box key={key}>
+          <Typography variant="h6" sx={{ pl: 1 }} color="white" gutterBottom>
+            {key}
+          </Typography>
+          <Grid
+            container
+            direction="row"
+            sx={{ justifyContent: 'start', mb: 2 }}
+            spacing={2}
+          >
+            {mappedData[key].map((digest) => (
+              <Grid key={digest.id} item xs={12} sm={6} md={4} lg={3}>
+                <DigestCard digest={digest} sx={{ height: '100%' }} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Box>
+      ))}
     </Shell>
   );
 }
