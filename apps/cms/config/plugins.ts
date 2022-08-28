785856055510de672b8cc45b0ce1fd648b92673b
Valiantsin <valiantsin.haradkou@icloud.com>
2022-08-28 23:14:11 +0300

    feat(cms): start using api transformer plugin

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -10,4 +10,14 @@ export default ({ env }) => ({
       },
     },
   },
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: '/api/',
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    },
+  },
 });
