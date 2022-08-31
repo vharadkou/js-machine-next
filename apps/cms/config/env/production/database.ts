73ff86864b8c219533f968b3b902a90654dd877a
Valiantsin <valiantsin.haradkou@icloud.com>
2022-08-31 17:50:25 +0300

    build(cms): revert HOST and PORT for DB

--- apps/cms/config/env/production/database.ts
+++ apps/cms/config/env/production/database.ts
@@ -2,6 +2,8 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
+      host: env('DATABASE_HOST'),
+      port: env('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USER'),
       password: env('DATABASE_PASSWORD'),
