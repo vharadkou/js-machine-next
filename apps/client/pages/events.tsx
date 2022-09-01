d5c10a69606edc2ae2d15c6d4b9ecbaf9ecdd9e1
Valiantsin <valiantsin.haradkou@icloud.com>
2022-09-01 18:42:12 +0300

    feat(client): card animation for events

--- apps/client/pages/events.tsx
+++ apps/client/pages/events.tsx
@@ -1,4 +1,5 @@
 import Grid from '@mui/material/Grid';
+import Grow from '@mui/material/Grow';
 import { EventCard } from '../components/EventCard';
 import { Shell } from '../components/Layout/Shell';
 import EventsJpg from '../public/images/events.jpg';
@@ -24,14 +25,27 @@ function Events() {
         sx={{ justifyContent: { sx: 'start', md: 'center' }, mb: 2 }}
         spacing={2}
       >
-        {data?.map((event) => (
-          <Grid key={event.id} item xs={12} sm={6} md={4} lg={3}>
-            <EventCard event={event} sx={{ height: '100%' }} />
-          </Grid>
+        {data?.map((event, index) => (
+          <Grow
+            in
+            key={event.id}
+            style={{ transformOrigin: '0 0 0' }}
+            timeout={animationDelayByIndex(index)}
+          >
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <EventCard event={event} sx={{ height: '100%' }} />
+            </Grid>
+          </Grow>
         ))}
       </Grid>
     </Shell>
   );
+}
+
+const BASE_ANIMATION_DELAY = 500;
+
+function animationDelayByIndex(index: number) {
+  return BASE_ANIMATION_DELAY * Math.log(index + 2);
 }
 
 export default Events;
