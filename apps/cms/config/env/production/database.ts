d6057dd632dce3a6419411508b19fb72cbb8f5d9
Valiantsin <valiantsin.haradkou@icloud.com>
2022-09-01 09:45:53 +0300

    build(cms): remove HOST and PORT from prod

--- apps/cms/config/env/production/database.ts
+++ apps/cms/config/env/production/database.ts
@@ -2,8 +2,6 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USER'),
       password: env('DATABASE_PASSWORD'),
