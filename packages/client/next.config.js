cb1a786f9d9cf44783b03894dee984ddf54e9e8b
Valiantsin <valiantsin.haradkou@icloud.com>
2023-09-21 10:25:37 +0200

    build: output standalone

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  output: 'standalone',
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
